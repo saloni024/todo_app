5d60e5301c22fe3242389e1662e7022dd51c0331
saloni024 <saloniprajapati024@gmail.com>
2021-04-26 03:47:44 -0400

    Complete todo app

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,10 +1,15 @@
 import React from "react"
 
 function ToDoItem(props){
+    const style = {
+        color: "gray",
+        textDecoration: "line-through",
+        fontStyle: "italic"
+    }
     return (
         <div className="todo-item">
             <input type="checkbox" checked={props.item.status} onChange={() => props.CheckBox(props.item.id)}/>
-            <p>{props.item.task}</p>
+            <p style={props.item.status ? style : null}>{props.item.task}</p>
         </div>
     )
 }
